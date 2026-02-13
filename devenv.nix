59b138261edec873ef0d713988c0ca97d0537e62
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-02-13 21:39:15 +0200

    Disable markdownlint in devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -101,7 +101,7 @@
     comrak.enable = true;
     deadnix.enable = true;
     detect-private-keys.enable = true;
-    markdownlint.enable = true;
+    # markdownlint.enable = true;
     mixed-line-endings.enable = true;
     nixfmt.enable = true;
     prettier.enable = true;
